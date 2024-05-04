b1e21991bd418d7f98ee15d762abfc6ea2a65433
rashiniashinsana <rashinivithanage2@gmail.com>
2024-05-04 21:47:50 +0530

    edited

--- script.js
+++ script.js
@@ -29,15 +29,6 @@ themeToggleBtn.onclick = () => {
     isDark = !isDark;
 }
 
-// const themeToggleBtn = document.querySelector(".theme-toggler");
-// const calculator = document.querySelector(".calculator");
-// const toggleIcon = document.querySelector(".toggler-icon");
-// let isDark = true;
-// themeToggleBtn.onclick = () => {
-//   calculator.classList.toggle("dark");
-//   themeToggleBtn.classList.toggle("active");
-//   isDark = !isDark;
-// };
 
 
 
